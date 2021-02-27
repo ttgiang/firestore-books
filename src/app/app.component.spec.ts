c29e8f9d4e30839cfc408176c3c8a02aa573255a
ttgiang <ttgiang@gmail.com>
2021-02-26 15:08:16 -1000

    Bootstrap UI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular10FirestoreCrud'`, () => {
+  it(`should have as title 'Angular10FirestoreBooks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular10FirestoreCrud');
+    expect(app.title).toEqual('Angular10FirestoreBooks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular10FirestoreCrud app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'Angular10FirestoreBooks app is running!'
+    );
   });
 });
