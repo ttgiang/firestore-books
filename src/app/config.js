fb6f984f3ec15a5c95d796cdee3240fdfac42d6b
ttgiang <ttgiang@gmail.com>
2021-03-09 11:14:45 -1000

    Import JSON

---
+++ src/app/config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyANj5NL2TKLNgPAbjxjZMfyPdSzYqzHhTU",
+  authDomain: "dots-d341f.firebaseapp.com",
+  projectId: "dots-d341f",
+  storageBucket: "dots-d341f.appspot.com",
+  messagingSenderId: "863585945443",
+  appId: "1:863585945443:web:ece39ca04072b2d196f556",
+  // databaseURL: "DATABASE_URL_HERE",
+};
+// Exports
+module.exports = firebaseConfig;
